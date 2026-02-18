3eb64b763b663f4427cea457724b858601d69f9f
bagaskrmn <bagaskaramnoer@gmail.com>
2026-02-18 15:34:57 +0700

    fix variable naming

--- internal/template/routes/routes-template.go
+++ internal/template/routes/routes-template.go
@@ -18,16 +18,16 @@ import (
 )
 
 func add%[2]sRoutes(f fiber.Router, m *module.Module) {
-	%[2]sSvc := %[1]sSvc.NewService(m.GetDB(), m.GetLogger())
+	%[1]sSvc := %[1]sSvc.NewService(m.GetDB(), m.GetLogger())
 	mdw := middleware.NewEnsureToken(m.GetDB())
 
-	%[2]sGroup := f.Group("/%[1]s", mdw.ValidateToken(), middleware.RateLimiterMiddleware(m.GetRuntimeConfig(), true))
+	%[1]sGroup := f.Group("/%[1]s", mdw.ValidateToken(), middleware.RateLimiterMiddleware(m.GetRuntimeConfig(), true))
 
-	%[2]sGroup.Get("", %[2]sHandlers.ReadList%[2]sApp(%[1]sSvc, m.GetLoggerFile()))
-	%[2]sGroup.Get("/:id", %[2]sHandlers.ReadDetail%[2]sApp(%[1]sSvc, m.GetLoggerFile()))
-	%[2]sGroup.Post("", %[2]sHandlers.Create%[2]sApp(%[1]sSvc, m.GetValidator(), m.GetLoggerFile()))
-	%[2]sGroup.Put("/:id", %[2]sHandlers.Update%[2]sApp(%[1]sSvc, m.GetValidator(), m.GetLoggerFile()))
-	%[2]sGroup.Delete("/:id", %[2]sHandlers.Delete%[2]sApp(%[1]sSvc, m.GetLoggerFile()))
+	%[1]sGroup.Get("", %[2]sHandlers.ReadList%[2]sApp(%[1]sSvc, m.GetLoggerFile()))
+	%[1]sGroup.Get("/:id", %[2]sHandlers.ReadDetail%[2]sApp(%[1]sSvc, m.GetLoggerFile()))
+	%[1]sGroup.Post("", %[2]sHandlers.Create%[2]sApp(%[1]sSvc, m.GetValidator(), m.GetLoggerFile()))
+	%[1]sGroup.Put("/:id", %[2]sHandlers.Update%[2]sApp(%[1]sSvc, m.GetValidator(), m.GetLoggerFile()))
+	%[1]sGroup.Delete("/:id", %[2]sHandlers.Delete%[2]sApp(%[1]sSvc, m.GetLoggerFile()))
 }
 
 `, nameLower, entity, repoName)
